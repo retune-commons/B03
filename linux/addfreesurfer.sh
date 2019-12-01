43ad4aab4025b6d0d571712fc557b0270508fff7
Wolf-Julian Neumann <50333569+neumann-wj@users.noreply.github.com>
2019-12-01 23:13:39 +0100

    test

--- linux/addfreesurfer.sh
+++
@@ -1,6 +0,0 @@
-echo
-export FREESURFER_HOME=/usr/local/freesurfer/
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
-export SUBJECTS_DIR=/mnt/c/tmp/
-
-
--- linux/test.sh
+++
@@ -1,5 +0,0 @@
-export FREESURFER_HOME=/usr/local/freesurfer/
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
-export SUBJECTS_DIR=/mnt/c/tmp/
-
-
